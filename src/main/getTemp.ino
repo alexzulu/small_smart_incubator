df4b9fad211665e4b3e3dbd33c76339c4d108869
Unknown <ua6hdw@gmail.com>
2018-05-05 22:10:05 +0300

    Вынос констант. Переделал немного функцию получения температуры.

--- src/main/getTemp.ino
+++ src/main/getTemp.ino
@@ -1,30 +1,32 @@
-void getTemp (){
+float getTemp (){
   float celsius;
   sensors.requestTemperatures();
   celsius = sensors.getTempC(TL0);
-  if(DEBUG == 1 && cCount == 0){
+  if(DEBUG == 1){
     Serial.print("Temp C: ");
     Serial.println(celsius);
   }
-  realTemp = celsius;
+  return celsius;
 }
 
-float old;
 void printTempLCD(){
-  float current = realTemp;
-  if(old != current){
+  float current = realTemperature;
+  if(oldTemperature != current){
     lcd.setCursor(9, 1);
     lcd.print("     ");
     lcd.setCursor(9, 1);
     lcd.print(current);
-    old = current;
+    oldTemperature = current;
   }
+}
+
+void printHeaterStateLCD(){
   lcd.setCursor(15, 0);
   lcd.print(heaterState);
 }
 
 void tempControl(){
-  if(realTemp < setTemp){
+  if(realTemperature < setTemperature){
     if(heaterState == 0 && digitalRead(HEATER_PIN) == LOW){
       digitalWrite(HEATER_PIN,HIGH);
       heaterState = 1;
@@ -33,7 +35,7 @@ void tempControl(){
       }
     }
   }
-  if(realTemp >= setTemp){
+  if(realTemperature >= setTemperature){
     if(heaterState == 1 && digitalRead(HEATER_PIN) == HIGH){
       digitalWrite(HEATER_PIN,LOW);
       heaterState = 0;
--- src/main/main.ino
+++ src/main/main.ino
@@ -4,6 +4,10 @@
 #include <DallasTemperature.h>
 #include <LCD_1602_RUS.h>
 
+const float setTemperature = 37.80;//Установленная температура
+const bool DEBUG = 0;//Вкл/откл режим отладки
+//const int controlDelay;//Задержка опроса.
+
 LCD_1602_RUS lcd(0x27, 16, 2);
 
 #define ONE_WIRE_BUS 9 //Нога на которой работает шина
@@ -14,33 +18,30 @@ DallasTemperature sensors(&oneWire);
 DeviceAddress TL0 = { 0x28, 0x6C, 0x91, 0xA9, 0x06, 0x00, 0x00, 0x31 };//Адрес датчика
 
 #define SENSOR_PLUS 8//Питание датчика +
-#define SENSOR_MINUS 10//Питание датчика -
-#define LCD_PLUS 12//Питание экрана.
+//#define SENSOR_MINUS 10//Питание датчика -
+//#define LCD_PLUS 12//Питание экрана.
 #define HEATER_PIN 7//Пин управления реле
 
-float setTemp;//Установленная температура
-float realTemp;//Реальная температура
+float oldTemperature;//Предыдущее замерянное значение температуры
+float realTemperature;//Реальная температура
 bool heaterState = 0;//Состояние нагревателя
-bool DEBUG = 0;//Вкл/откл режим отладки
-//int controlDelay;//Задержка опроса.
-int cCount = 0;
 
 void setup() {
   //Инициализация пинов
-  pinMode(SENSOR_MINUS,OUTPUT);
-  digitalWrite(SENSOR_MINUS,LOW);
+//  pinMode(SENSOR_MINUS,OUTPUT);
+//  digitalWrite(SENSOR_MINUS,LOW);
   pinMode(SENSOR_PLUS,OUTPUT);
   digitalWrite(SENSOR_PLUS,HIGH);
   
-  pinMode(LCD_PLUS,OUTPUT);
-  digitalWrite(LCD_PLUS,HIGH);
+//  pinMode(LCD_PLUS,OUTPUT);
+//  digitalWrite(LCD_PLUS,HIGH);
   
   pinMode(HEATER_PIN,OUTPUT);
   digitalWrite(HEATER_PIN,LOW);
 
   wdt_enable(WDTO_8S);//Включаем watch dog timer и ставим период 8 секунд
 
-  setTemp = 37.80;//Устанавливаем температуру
+//  setTemperature = 37.80;//Устанавливаем температуру
 //  controlDelay = 5000;
     
   Serial.begin(9600);
@@ -60,12 +61,9 @@ void setup() {
 }
 
 void loop(void) {
-  if(cCount > 19){
-    cCount = 0;
-  }
-  getTemp();
+  realTemperature = getTemp();
   tempControl();
   printTempLCD();
-  cCount++;
+  printHeaterStateLCD();
   wdt_reset();//Сбрасываем wdt каждый цикл
 }
